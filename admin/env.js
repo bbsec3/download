e1df013c4e67e32556a847805756323d5425f972
bbsec3 <45933542+bbsec3@users.noreply.github.com>
2020-03-27 20:16:04 +0800

    Add files via upload

---
+++ admin/env.js
@@ -0,0 +1,16 @@
+window.v2board = {
+  // 站点标题
+  title: 'Bilibili',
+  // 站点描述
+  description: 'Bilibili is best',
+  // API
+  host: 'https://kms.bbsec.xyz',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: ''
+}
